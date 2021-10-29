de583a8fd37a07339591ca186b2494ca84d39658
Sebastian Waisbrot <seppo0010@gmail.com>
2021-10-29 15:02:00 -0300

    initial commit, poc

---
+++ source/download-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for url in $(xsv select 'tender/documents/0/url' releases_documents_items.csv |sort |uniq|grep -v 'tender/documents/0/url'); do
+    path="${url//\//_}"
+    if [ ! -f "$path" ]; then
+        wget "$url" -O "data/$path"
+    else
+        echo "$url already downloaded" >&2
+    fi
+done
