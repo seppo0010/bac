bef5b1b7146c163f192e333eaf8d270757856bf8
Sebastian Waisbrot <seppo0010@gmail.com>
2021-10-29 16:22:44 -0300

    fix cache path

--- source/download-all.sh
+++ source/download-all.sh
@@ -2,7 +2,7 @@
 
 for url in $(xsv select 'tender/documents/0/url' releases_documents_items.csv |sort |uniq|grep -v 'tender/documents/0/url'); do
     path="${url//\//_}"
-    if [ ! -f "$path" ]; then
+    if [ ! -f "data/$path" ]; then
         wget "$url" -O "data/$path"
     else
         echo "$url already downloaded" >&2
